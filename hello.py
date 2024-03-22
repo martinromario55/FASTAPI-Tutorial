7d7359d784c9c47e894714f3e91d89809e9b1107
martinromario55 <martinromario55@gmail.com>
2024-03-22 08:14:19 +0300

    Body

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get('/hi')
-def greet(who):
+@app.post('/hi')
+def greet(who:str = Body(embed=True)):
     return f"Hello? {who}?"
