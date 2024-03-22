710b8cf0155f9f3019b8d10d00b2e7975556a854
martinromario55 <martinromario55@gmail.com>
2024-03-22 08:04:22 +0300

    Query Parameters

--- hello.py
+++ hello.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/hi/{who}')
+@app.get('/hi')
 def greet(who):
     return f"Hello? {who}?"
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("hello:app", reload=True)
