fee229c65f9b65732a0a73d51e80b156077da376
martinromario55 <martinromario55@gmail.com>
2024-03-22 08:42:29 +0300

    Type Conversion

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 @app.post('/hi')
-def greet(who:str = Body(embed=True)):
+def greet(who:str = Header()):
     return f"Hello? {who}?"
+
+@app.post('/agent')
+def get_agent(user_agent:str = Header()):
+    return user_agent
