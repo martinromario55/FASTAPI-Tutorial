14881dfb7d1964474f06c49b264014048f3ccb53
martinromario55 <martinromario55@gmail.com>
2024-03-22 08:00:16 +0300

    URL Path

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/hi')
-def greet():
-    return "Hello? World?"
+@app.get('/hi/{who}')
+def greet(who):
+    return f"Hello? {who}?"
 
 if __name__ == "__main__":
     import uvicorn
