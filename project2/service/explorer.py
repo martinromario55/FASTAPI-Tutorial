18d1d992119ddc1d59753c2a86ee7f4184097cb7
martinromario55 <martinromario55@gmail.com>
2024-03-26 07:24:51 +0300

    Data Layer

--- project2/service/explorer.py
+++ project2/service/explorer.py
@@ -1,1 +1,20 @@
-from fake import explorer, creature
+from model.explorer import Explorer
+from data.explorer import data
+
+def get_all() -> list[Explorer]:
+    return data.get_all()
+
+def get_one(name: str) -> Explorer | None:
+    return data.get(name)
+
+def create(explorer: Explorer) -> Explorer:
+    return data.create(explorer)
+
+def replace(id,explorer: Explorer) -> Explorer:
+    return data.replace(id, explorer)
+
+def modify(id, explorer: Explorer) -> Explorer:
+    return data.modify(id, explorer)
+
+def delete(id, explorer: Explorer) -> bool:
+    return data.delete(id)
