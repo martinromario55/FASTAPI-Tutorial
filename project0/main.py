53314498e3b5b450a55192aa335da619ade3b939
martinromario55 <martinromario55@gmail.com>
2024-03-27 05:58:34 +0300

    Using Static data

--- project0/main.py
+++ project0/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -10,6 +11,23 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+    
+my_posts = [
+    {
+        "id": 1,
+        "title": "My first post",
+        "content": "This is my first post",
+        "published": True,
+        "rating": 10
+    },
+    {
+        "id": 2,
+        "title": "My second post",
+        "content": "This is my second post",
+        "published": True,
+        "rating": 10
+    }
+]
 
 
 @app.get("/")
@@ -18,10 +36,18 @@ def idex():
 
 @app.get('/posts')
 def get_posts():
-    return {"data": "These are your posts"}
+    return {"data": my_posts}
 
-@app.post('/createposts')
+@app.post('/posts')
 def create_posts(new_post: Post):
-    # print(new_post)
-    print(new_post.model_dump())
-    return {"data": "new_post"}
+    post_dict = new_post.model_dump()
+    post_dict["id"] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get('/posts/{id}')
+def get_post(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return {"data": post}
+    return {"message": "Post not found"}
