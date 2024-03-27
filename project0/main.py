d497bc6dff6b12b9675b8d66813ee1615736979f
martinromario55 <martinromario55@gmail.com>
2024-03-27 05:37:36 +0300

    Data validation and schema

--- project0/main.py
+++ project0/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Post Schema
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 def idex():
@@ -11,6 +21,7 @@ def get_posts():
     return {"data": "These are your posts"}
 
 @app.post('/createposts')
-def create_posts(playload: dict = Body(...)):
-    print(playload)
-    return {"new_post": f"title {playload['title']} content: {playload['content']}"}
+def create_posts(new_post: Post):
+    # print(new_post)
+    print(new_post.model_dump())
+    return {"data": "new_post"}
