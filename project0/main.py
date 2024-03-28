f4c76272cb938e9f27368ac4f40b0b4eb2b8c1d8
martinromario55 <martinromario55@gmail.com>
2024-03-28 08:32:30 +0300

    Creating a User

--- project0/main.py
+++ project0/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Body, Response, status, HTTPException, Depends
-from .schemas import Post
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
+from typing import List
+
+from .schemas import PostCreate, Post, UserCreate, UserOut
 from . import models
 from .database import engine, get_db
 
@@ -12,93 +12,40 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-    
-# Postgres Init
-try:
-    conn = psycopg2.connect(
-        host="localhost",
-        user="postgres",
-        password="202022141057",
-        port="5432",
-        database="fastapidb"
-    )
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    print('Database connection was successfull!')
-    
-    
-
-except Exception as Error:
-    print("Connection to database failed")
-    print('Error:', Error)
-    
-my_posts = [
-    {
-        "id": 1,
-        "title": "My first post",
-        "content": "This is my first post",
-        "published": True,
-        "rating": 10
-    },
-    {
-        "id": 2,
-        "title": "My second post",
-        "content": "This is my second post",
-        "published": True,
-        "rating": 10
-    }
-]
 
 @app.get("/")
 def index():
     return {"message": "Hello World!"}
 
-@app.get('/sqlalchemy')
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
 # Get All Posts
-@app.get('/posts')
+@app.get('/posts', response_model=List[Post])
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     # print(posts)
-    return {"data": posts}
+    return posts
 
 # Create Post
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """, 
-    #                (post.title, post.content, post.published))
-    
-    # new_post = cursor.fetchone()
-    # conn.commit()
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=Post)
+def create_posts(post: PostCreate, db: Session = Depends(get_db)):
     
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     
-    return {"new_post": new_post}
+    return new_post
 
 # Get Post by Id
-@app.get('/posts/{id}')
+@app.get('/posts/{id}', response_model=Post)
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
-    # # print(post)
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
-        # response.status_code = status.HTTP_404_NOT_FOUND
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    return {"post_detail": post}
+    return post
 
 # Delete Post    
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
     
     post = db.query(models.Post).filter(models.Post.id == id)
     
@@ -111,11 +58,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # Update Post
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
-    #                (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
+@app.put("/posts/{id}", response_model=Post)
+def update_post(id: int, updated_post: PostCreate, db: Session = Depends(get_db)):
     
     post_query = db.query(models.Post).filter(models.Post.id == id)
     
@@ -129,3 +73,14 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     db.commit()
     
     return {"data": post_query.first()}
+
+
+# Create new User
+@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
