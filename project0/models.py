3a7e9bcb11286adaab1240e8308d9519557544aa
martinromario55 <martinromario55@gmail.com>
2024-04-08 06:35:10 +0300

    Advanced Queries

--- project0/models.py
+++ project0/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -10,8 +11,11 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE')
-    created_at = Column(TIMESTAMP(timezone=True),nullable=True, server_default=text('now()'))
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    
+    owner = relationship("User")
     
     
 class User(Base):
--- project0/routers/post.py
+++ project0/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 from ..schemas import PostCreate, Post
 from .. import models
@@ -14,17 +14,31 @@ router = APIRouter(
 
 # Get All Posts
 @router.get('/', response_model=List[Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    # Get all posts
+    # posts = db.query(models.Post).all()
     # print(posts)
+    
+    # Get all posts from current user
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    
+    # limit posts
+    # posts = db.query(models.Post).limit(limit).all()
+    
+    # skip posts
+    # posts = db.query(models.Post).offset(skip).limit(limit).all()
+    
+    # search posts
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).all()
+    
     return posts
 
 # Create Post
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=Post)
 def create_posts(post: PostCreate, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     
-    print(current_user.email)
-    new_post = models.Post(**post.model_dump())
+    # print(current_user.email)
+    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -43,12 +57,18 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
     
-    if post.first() == None:
+    post = post_query.first()
+    
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     
-    post.delete(synchronize_session=False)
+    # Check if current user owns the post
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not the owner of this post")
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -63,6 +83,10 @@ def update_post(id: int, updated_post: PostCreate, db: Session = Depends(get_db)
     
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    # Check if current user owns the post
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not the owner of this post")
     
     post_query.update(updated_post.model_dump(), synchronize_session=False)
     
